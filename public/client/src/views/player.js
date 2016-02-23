8138f3b4306cc0134585003b318651d3af17e8cd
Roman Pekarskiy <roman.pekarskiy@gmail.com>
2016-02-23 23:39:46 +0600

    added mediaelement HLS player. Default HTML5 Video cannot play HLS

--- public/client/src/views/player.js
+++ public/client/src/views/player.js
@@ -7,12 +7,29 @@ let PlayerView = Backbone.View.extend({
 		this.preloader = new PreloaderView({player:this});
 		this.el.appendChild(this.videoElement);
 		this.el.appendChild(this.preloader.el);
-	},
-	render: function(){
 
+		this._play = this.play;
+		this.play = this.firstPlay;
+	},
+	firstPlay: function( location ){
+		var self = this;
+		this.videoElement.src = location;
+		this.mediaElement = new MediaElement(this.videoElement, {
+			pluginPath: 'mediaelement/',
+			flashName: 'flashmediaelement.swf',
+			silverlightName: 'silverlightmediaelement.xap',
+			defaultVideoWidth: '100%',  
+			defaultVideoHeight: '100%',
+			timerRate: 250,
+			success: (mediaElement, domObject) => { 
+			    this.mediaElement.play();
+			    this.play = this._play;
+			},
+		});
 	},
 	play: function( location ){
-		this.videoElement.play(location);
+		this.mediaElement.setSrc(location);
+		this.mediaElement.play();
 	}
 });
 
