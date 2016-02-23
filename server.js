655cb61207b971bf73673de3cb10bae7d386b34a
Roman Pekarskiy <roman.pekarskiy@gmail.com>
2016-02-23 20:18:12 +0600

    move client files to public dir

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use('/client', express.static('client'));
+app.use('/client', express.static('public/client'));
+app.use('/admin', express.static('public/admin'));
 
 app.listen(8080);
 console.log('ready at '+ 8080 +' port');
