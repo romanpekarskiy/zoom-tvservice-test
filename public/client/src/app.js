51cd600e97e3862f8550d162c3a491239f68995b
Roman Pekarskiy <roman.pekarskiy@gmail.com>
2016-02-24 14:40:18 +0600

    channels from server

--- public/client/src/app.js
+++ public/client/src/app.js
@@ -2,17 +2,20 @@ import ChannelCollection from 'models/channelCollection';
 import ChannelModel from 'models/channel';
 import AppView from 'views/app';
 
-let channelCollection = new ChannelCollection([
-	new ChannelModel({title:'ART',location:'http://live.art.cdnvideo.ru/art-pull//ed6b7e08ea/playlist.m3u8'}),
-	new ChannelModel({title:'КПРФ',location:'http://kprf-live.cdn.ngenix.net/live/_definst_/stream_high/playlist.m3u8'}),
-]);
+let channelCollection = new ChannelCollection();
 
-let app = new AppView({
-	collection: channelCollection
-});
+channelCollection.fetch({
+	success: function() {
+		let app = new AppView({
+			collection: channelCollection
+		});
 
-document.body.appendChild(app.el);
+		document.body.appendChild(app.el);
 
-app.render();
+		app.render();
 
-window.app = app;
+		window.app = app;
+	}
+})
+
+window.channelCollection = channelCollection;
--- public/client/src/models/channel.js
+++ public/client/src/models/channel.js
@@ -1,7 +1,10 @@
 import Backbone from 'backbone';
 
 let ChannelModel = Backbone.Model.extend({
-
+	defaults:{
+		title:'untitled',
+		location:''
+	}
 });
 
 export default ChannelModel
